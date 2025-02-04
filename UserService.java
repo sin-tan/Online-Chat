426f9db18bc0ed41227433dd4487749ef8024419
Tanishka Singh <112722128+sin-tan@users.noreply.github.com>
2025-02-04 23:40:57 +0530

    Update UserService.java

--- UserService.java
+++ UserService.java
@@ -1,130 +1,64 @@
-import java.util.*;
+import java.util.Scanner;
 
-class UserService {
-    private Map<Integer, User> usersById = new HashMap<>();
+public class OnlineChat {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        UserService userService = new UserService();
 
-    public void addUser(int userId, String name, String passHash) {
-        usersById.put(userId, new User(userId, name, passHash));
-    }
+        System.out.println("Welcome to the Online Chat!");
+        while (true) {
+            System.out.println("1. Add User\n2. Send Friend Request\n3. Approve Friend Request\n4. Send Message\n5. Exit");
+            System.out.print("Choose an option: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
 
-    public void removeUser(int userId) {
-        usersById.remove(userId);
-    }
-
-    public void addFriendRequest(int fromUserId, int toUserId) {
-        User fromUser = usersById.get(fromUserId);
-        User toUser = usersById.get(toUserId);
-        if (fromUser != null && toUser != null) {
-            toUser.receiveFriendRequest(fromUserId);
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter user ID: ");
+                    int userId = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.print("Enter username: ");
+                    String name = scanner.nextLine();
+                    userService.addUser(userId, name, "pass");
+                    System.out.println("User added successfully!");
+                    break;
+                case 2:
+                    System.out.print("Enter your user ID: ");
+                    int fromId = scanner.nextInt();
+                    System.out.print("Enter friend's user ID: ");
+                    int toId = scanner.nextInt();
+                    userService.addFriendRequest(fromId, toId);
+                    System.out.println("Friend request sent.");
+                    break;
+                case 3:
+                    System.out.print("Enter your user ID: ");
+                    int approverId = scanner.nextInt();
+                    System.out.print("Enter friend's user ID: ");
+                    int requesterId = scanner.nextInt();
+                    userService.approveFriendRequest(requesterId, approverId);
+                    break;
+                case 4:
+                    System.out.print("Enter your user ID: ");
+                    int senderId = scanner.nextInt();
+                    System.out.print("Enter friend's user ID: ");
+                    int receiverId = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.print("Enter message: ");
+                    String message = scanner.nextLine();
+                    User sender = userService.getUser(senderId);
+                    if (sender != null) {
+                        sender.messageUser(receiverId, message);
+                    } else {
+                        System.out.println("User not found.");
+                    }
+                    break;
+                case 5:
+                    System.out.println("Exiting chat application.");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid choice. Try again.");
+            }
         }
-    }
-
-    public void approveFriendRequest(int fromUserId, int toUserId) {
-        User fromUser = usersById.get(fromUserId);
-        User toUser = usersById.get(toUserId);
-        if (fromUser != null && toUser != null) {
-            toUser.approveFriendRequest(fromUserId);
-        }
-    }
-
-    public User getUser(int userId) {
-        return usersById.get(userId);
-    }
-}
-
-class User {
-    private int userId;
-    private String name;
-    private String passHash;
-    private Map<Integer, User> friends = new HashMap<>();
-    private Map<Integer, PrivateChat> privateChats = new HashMap<>();
-    private Map<Integer, GroupChat> groupChats = new HashMap<>();
-    private Map<Integer, AddRequest> receivedFriendRequests = new HashMap<>();
-
-    public User(int userId, String name, String passHash) {
-        this.userId = userId;
-        this.name = name;
-        this.passHash = passHash;
-    }
-
-    public void messageUser(int friendId, String message) {
-        if (friends.containsKey(friendId)) {
-            PrivateChat chat = privateChats.computeIfAbsent(friendId, k -> new PrivateChat(this, friends.get(friendId)));
-            chat.addMessage(new Message(message, new Date()));
-            System.out.println(name + " to " + friends.get(friendId).name + ": " + message);
-        } else {
-            System.out.println("User is not your friend.");
-        }
-    }
-
-    public void receiveFriendRequest(int friendId) {
-        receivedFriendRequests.put(friendId, new AddRequest(friendId, this.userId, RequestStatus.UNREAD, new Date()));
-        System.out.println("Friend request received from user " + friendId);
-    }
-
-    public void approveFriendRequest(int friendId) {
-        if (receivedFriendRequests.containsKey(friendId)) {
-            friends.put(friendId, new User(friendId, "Friend" + friendId, "hash")); // Example user
-            receivedFriendRequests.remove(friendId);
-            System.out.println("Friend request from " + friendId + " approved.");
-        }
-    }
-}
-
-abstract class Chat {
-    protected List<User> users = new ArrayList<>();
-    protected List<Message> messages = new ArrayList<>();
-
-    public void addMessage(Message message) {
-        messages.add(message);
-    }
-}
-
-class PrivateChat extends Chat {
-    public PrivateChat(User firstUser, User secondUser) {
-        this.users.add(firstUser);
-        this.users.add(secondUser);
-    }
-}
-
-class GroupChat extends Chat {
-    public void addUser(User user) {
-        users.add(user);
-    }
-}
-
-class Message {
-    private String message;
-    private Date timestamp;
-
-    public Message(String message, Date timestamp) {
-        this.message = message;
-        this.timestamp = timestamp;
-    }
-}
-
-enum RequestStatus {
-    UNREAD, READ, ACCEPTED, REJECTED;
-}
-
-class AddRequest {
-    private int fromUserId;
-    private int toUserId;
-    private RequestStatus requestStatus;
-    private Date timestamp;
-
-    public AddRequest(int fromUserId, int toUserId, RequestStatus requestStatus, Date timestamp) {
-        this.fromUserId = fromUserId;
-        this.toUserId = toUserId;
-        this.requestStatus = requestStatus;
-        this.timestamp = timestamp;
-    }
-
-    public RequestStatus getRequestStatus() {
-        return requestStatus;
-    }
-
-    public void setRequestStatus(RequestStatus requestStatus) {
-        this.requestStatus = requestStatus;
     }
 }
